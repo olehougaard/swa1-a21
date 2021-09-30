c99d91459206f97ae12eb9e7c51416946ebfd595
Ole Ildsgaard Hougaard <olehougaard@gmail.com>
2021-09-30 08:51:16 +0200

    Initial commit

---
+++ asynchronous/static/presenter_await.js
@@ -0,0 +1,27 @@
+export default (init_model, view) => {
+  let model = init_model
+
+  const onAction = async ({type, ...params}) =>  {
+    switch(type) {
+      case 'hire':
+        const { id } = params
+        const salary = view.prompt('Salary?')
+        if (salary) {
+          try {
+            const headers = { 'Content-Type': 'application/json', Accept: 'application/json' }
+            const employeeResponse = await fetch('http://localhost:9090/employees', { method: 'POST', body: JSON.stringify({salary, manager:false}), headers })
+            const employee = await employeeResponse.json()
+            const personResponse = await fetch('http://localhost:9090/persons/' + id, { method: 'PATCH', body: JSON.stringify(employee), headers })
+            const person = await personResponse.json()
+            model = model.addEmployee(employee).updatePerson(person)
+            view.update(model)
+          } catch (e) {
+            console.log(e)
+          }
+        }
+        break;
+    }
+  }
+
+  return { onAction }
+}
