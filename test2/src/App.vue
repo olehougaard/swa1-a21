e73031f73005d2059d321e4f87fec9d55030098b
Ole Ildsgaard Hougaard <olehougaard@gmail.com>
2021-11-05 06:07:35 +0100

    initial commit

---
+++ test2/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="dynamic-component-demo" class="demo">
+    <h1>People</h1>
+    <personnel :persons='persons' @hire='hire'/>
+  </div>
+</template>
+
+<script>
+import Personnel from "@/components/Personnel"
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      persons: [{id: 1, name: "Joe"}, {id: 2, name:"Liz", employeeId: 17}]
+    }
+  },
+  methods: {
+    hire({id}) {
+      this.$window.alert('id ' + id)
+    }
+  },
+  components: {
+    Personnel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ test2/src/components/Personnel.vue
@@ -0,0 +1,34 @@
+<template>
+  <table>
+      <thead><tr><td>Id</td><td>Name</td><td>Employee id</td><td>Salary</td><td>Manager</td></tr></thead>
+      <tbody>
+          <tr v-for="p in persons" :key="p.id">
+              <td>{{ p.id }} </td>
+              <td>{{ p.name }} </td>
+              <template v-if="p.employeeId">
+                  <td>{{ p.employeeId }}</td>
+                  <td>{{ p.salary || 0 }}</td>
+                  <td>{{ Boolean(p.manager) }}</td>
+              </template>
+              <template v-else>
+                  <td colspan = "3"><button v-on:click="hire(p)">Hire</button></td>
+              </template>
+          </tr>
+      </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  emits: ['hire'],
+  name: "Personnel",
+  props: {
+    persons: Array
+  },
+  methods: {
+    hire(p) {
+      this.$emit('hire', p)
+    }
+  }
+}
+</script>
